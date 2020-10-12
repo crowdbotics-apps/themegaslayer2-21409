557b244ed08117b3df3a0225c4b81b48535a1324
Crowdbotics <team@crowdbotics.com>
2020-10-12 17:54:37 +0000

    Update components

--- src/features/BlankScreen0139180/screens/blank.js
+++ src/features/BlankScreen0139180/screens/blank.js
@@ -1,23 +1,56 @@
-import React from "react";
-import { View, Image, ImageBackground, TouchableOpacity } from "react-native";
-import { withStyles, Text, Button, CheckBox, Radio, Toggle, Icon, Input, Datepicker } from 'react-native-ui-kitten';
-import Slider from '@react-native-community/slider';
+import React from "react"
+import { View, Image, ImageBackground } from "react-native"
+import {
+  withStyles,
+  Text,
+  Button,
+  CheckBox,
+  Radio,
+  Toggle,
+  Icon,
+  Input,
+  Datepicker
+} from "react-native-ui-kitten"
+import Slider from "@react-native-community/slider"
+
 import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export class _Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
 
-  state = { }
+  state = {}
 
-  render = () => <View style={this.props.themedStyle.container}></View>;
+  render = () => (
+    <ImageBackground
+      source={{
+        uri:
+          "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/download_1_44ze5kE.png"
+      }}
+      style={styles.ImageBackground_1}
+    >
+      <Button
+        title="menu"
+        color="#3366FF"
+        style={styles.Button_3}
+        onPress={() => alert("Pressed!")}
+      />
+      <Image resizeMode="cover" style={styles.Image_11} />
+      <Button
+        title="start button "
+        color="#3366FF"
+        style={styles.Button_7}
+        onPress={() => alert("Pressed!")}
+      />
+    </ImageBackground>
+  )
 }
 
 export default Blank = withStyles(_Blank, theme => ({
   container: {
     backgroundColor: theme["color-basic-100"]
   }
-}));
+}))
